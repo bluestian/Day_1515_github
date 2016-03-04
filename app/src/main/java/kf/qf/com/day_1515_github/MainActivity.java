24f73144e0f660f10404fefa7d153de7313d3359
unknown <>
2016-03-04 15:24:49 +0800

    Initial commit

---
+++ app/src/main/java/kf/qf/com/day_1515_github/MainActivity.java
@@ -0,0 +1,13 @@
+package kf.qf.com.day_1515_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
